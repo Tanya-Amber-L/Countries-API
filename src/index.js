93191aa454d298cf761bf1f34a8626c7eb163026
Tanya-Amber-L <tanya.leenders@gmail.com>
2022-04-08 11:47:19 +0200

    setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { render } from "react-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+
+render(<App />, document.getElementById("root"));

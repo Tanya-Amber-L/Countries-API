252c5ff61ecfda157efc86fe6455f15ee355d400
Tanya <Leenders>
2022-04-21 14:57:20 +0200

    Advancing on search bar

--- src/components/borderCountry.js
+++ src/components/borderCountry.js
@@ -1,32 +1,33 @@
 import data from "../data/countries.json";
 
-export default function BorderCountry({ borderCodes }) {
-	// console.log(borderCodes);
-	if (!borderCodes) {
-		return;
-	}
+export default function BorderCountry({borderCodes}) {
+    // console.log(borderCodes);
+    if (!borderCodes) {
+        return;
+    }
 
-	let borderCountries = [];
+    let borderCountries = [];
 
-	borderCodes.forEach((code) => {
-		for (let item of data) {
-			if (item.code === code) {
-				borderCountries.push(item.name);
-				// console.log(item.name);
-				// return <li>{item.name}</li>;
-			}
-		}
-	});
+    borderCodes.forEach((code) => {
+        for (let item of data) {
+            if (item.code === code) {
+                borderCountries.push(item.name);
+                // console.log(item.name);
+                // return <li>{item.name}</li>;
+            }
+        }
+    });
 
-	return (
-		<ul className="border-list">
-			{borderCountries.map((country) => {
-				return (
-					<li className="border-item" key={country}>
-						{country}
-					</li>
-				);
-			})}
-		</ul>
-	);
+    return (
+        <ul className="border-list">
+            {borderCountries.map((country) => {
+                return (
+                    <li className="border-item" key={country}>
+                        {country}
+                        <div className="test"></div>
+                    </li>
+                );
+            })}
+        </ul>
+    );
 }
--- src/routes/home.js
+++ src/routes/home.js
@@ -1,53 +1,41 @@
-import { useState } from "react";
-import { fetchAllCountries, fetchCountriesFromRegion } from "../api/fetch";
+import {useEffect, useState} from "react";
+import {fetchAllCountries, fetchCountriesFromRegion} from "../api/fetch";
 import CountryCard from "../components/countryCard";
 import RegionFilter from "../components/regionFilter";
 import SearchBar from "../components/searchBar";
 
 export default function HomePage() {
-	let [countries, setCountries] = useState([]);
-	let [region, setRegion] = useState("all");
-	const [inputText, setInputText] = useState("");
+    let [countries, setCountries] = useState([]);
+    let [region, setRegion] = useState("all");
+    const [inputText, setInputText] = useState("");
 
-	// let inputHandler = (e) => {
-	// 	var lowerCase = e.target.value.toLowerCase();
-	// 	setInputText(e.target.value.toLowerCase());
-	// 	console.log(inputText);
-	// 	console.log(e.target.value.toLowerCase());
-	// };
-	// console.log(inputText);
+    useEffect(() => {
+        if (region === "all") {
+            fetchAllCountries(setCountries)
+        } else {
+            fetchCountriesFromRegion(region).then((data) => {
+                setCountries(data);
+            });
+        }
+    }, [region])
 
-	if (region === "all") {
-		fetchAllCountries(setCountries);
-	} else {
-		fetchCountriesFromRegion(region).then((data) => {
-			setCountries(data);
-		});
-	}
+    const filterCountries = (countries) => {
+        const filteredCountries = countries.filter((country) => {
+            return country.name.common.toLowerCase().includes(inputText.toLowerCase())
+        })
+        setCountries(filteredCountries)
+    };
 
-	let filterCountries = () => {
-		console.log(inputText);
-		// countries.filter((country) => {
-		// 	if (inputText === "") {
-		// 		return country;
-		// 	} else if (
-		// 		country.name.common
-		// 			.toLowerCase()
-		// 			.includes(inputText.toLowerCase())
-		// 	) {
-		// 		return country;
-		// 	}
-		// });
-		// console.log(countries);
-	};
+    useEffect(() => {
+        if (inputText !== "") {
+            filterCountries(countries)
+        }
+        console.log(inputText)
+    }, [filterCountries, inputText])
 
-	filterCountries();
-
-	return (
-		<>
-			<SearchBar setInputText={setInputText} />
-			<RegionFilter setRegion={setRegion} />
-			<CountryCard countries={countries} />
-		</>
-	);
+    return (<>
+        <SearchBar setInputText={setInputText}/>
+        <RegionFilter setRegion={setRegion}/>
+        <CountryCard countries={countries}/>
+    </>);
 }
